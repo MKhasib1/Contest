d563922da379e1c2ea112b0516b7c6ee632b88be
mac <ali.qadomy@hotmail.com>
2019-06-29 18:42:57 +0300

    aadd steps counter

--- app/src/main/java/com/example/hackathon/WaterTracking.java
+++ app/src/main/java/com/example/hackathon/WaterTracking.java
@@ -2,15 +2,24 @@ package com.example.hackathon;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WaterTracking extends AppCompatActivity {
 
+
+    private TextView glassesDrinked, glassesNeeded, timerRemain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_tracking);
+
+
+        glassesDrinked = findViewById(R.id.glassesWaterDrinked);
+        glassesNeeded = findViewById(R.id.glassesWaterNeeded);
+        timerRemain = findViewById(R.id.timerTextView);
     }
 
     public void LeadershipActivity(View view) {
